f664601c5fb9c4d62b6358e213b3dc054c93e323
MarcoBernaus <marcobernaus04@gmail.com>
2026-07-08 11:46:41 -0300

    commit de cambios en classdiagram

---
+++ Model.Domain/ConsultaMedica.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Model.Domain
+{
+    public class ConsultaMedica
+    {
+        public int idConsultaMedica { get; private set; }
+
+        public string sintomas { get; private set; }
+
+        public string notasClinicas { get; private set; }
+
+        public string diagnostico { get; private set; }
+
+    }
+}
