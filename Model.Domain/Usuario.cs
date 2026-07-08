74c94670fde47531927a3061eca1dec9bde70ed2
AlejoMistra <mistralettialejo@gmail.com>
2026-07-08 11:52:48 -0300

    agrega usuario

---
+++ Model.Domain/Usuario.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Domain
+{
+    internal class Usuario
+    {
+        private enum Rol
+        {
+            Admin,
+            Administrativo,
+            Paciente,
+            Profesional
+        }
+
+        private enum Estado
+        {
+            Activo,
+            Inactivo
+        }
+
+        public int Id { get; private set; }
+
+        public string Nombre { get; private set; }
+
+        public string contraseña { get; private set; }
+
+        public Rol Rol { get; private set; }
+
+        public Estado Estado { get; private set; }
+    }
+}
