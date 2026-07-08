9bec7b409f3b6d07092e9f528f884072d10fadcf
AlejoMistra <mistralettialejo@gmail.com>
2026-07-08 11:23:18 -0300

    agrgando Model.Domain

---
+++ Model.Domain/Persona.cs
@@ -0,0 +1,15 @@
+﻿namespace Model.Domain
+{
+    internal class Persona
+    {
+        public string Nombre { get; private set; }
+
+        public string Apellido { get; private set; }
+        
+        public string NroDocumento { get; private set; }
+
+        public string Email { get; private set; }
+
+        public string Telefono { }
+    }
+}
