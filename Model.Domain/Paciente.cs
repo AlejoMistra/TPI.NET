0ab09a776bd357010713fbb96d606d3d21488a7b
MarcoBernaus <marcobernaus04@gmail.com>
2026-07-10 11:32:01 -0300

    commit de paciente

--- Model.Domain/Paciente.cs
+++ Model.Domain/Paciente.cs
@@ -13,5 +13,15 @@ namespace Model.Domain
         public DateTime FechaNacimiento { get; private set; }
 
         public string ObraSocial { get; private set; } = string.Empty;
+
+        public ICollection<Turno> Turnos { get; set; }
+
+        public HistoriaClinica historia_clinica { get; private set; }
+
+        public Paciente()
+        {
+            Turnos = new List<Turno>();
+            historia_clinica = new HistoriaClinica();
+        }
     }
 }
