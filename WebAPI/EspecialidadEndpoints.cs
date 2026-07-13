607898439ed69d39aa07139264ea17c95a3707da
MarcoBernaus <marcobernaus04@gmail.com>
2026-07-13 18:30:57 -0300

    endpoints + program.cs

---
+++ WebAPI/EspecialidadEndpoints.cs
@@ -0,0 +1,21 @@
+﻿using Application.Services;
+using DTOs;
+
+namespace WebAPI
+{
+    public static class EspecialidadEndpoints
+    {
+        public static void MapEspecialidadEndpoints(this WebApplication app)
+        {
+            app.MapGet("/especialidades", async () =>
+            {
+                EspecialidadService paisService = new EspecialidadService();
+                var dtos = await especialidadService.GetAllAsync();
+                return Results.Ok(dtos);
+            })
+            .WithName("GetAllEspecialidades")
+            .Produces<List<EspecialidadDTO>>(StatusCodes.Status200OK)
+            .WithOpenApi();
+        }
+    }
+}
--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,36 +1,32 @@
-using Microsoft.AspNetCore.Authentication.Negotiate;
+using WebAPI;
+using Application.Services;
+using Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddAuthentication(NegotiateDefaults.AuthenticationScheme)
-   .AddNegotiate();
-
-builder.Services.AddAuthorization(options =>
-{
-    // By default, all incoming requests will be authorized according to the default policy.
-    options.FallbackPolicy = options.DefaultPolicy;
-});
+// Add Dependency Injection
+builder.Services.AddScoped<IProfesionalRepository, ProfesionalRepository>();
+builder.Services.AddScoped<IProfesionalService, ProfesionalService>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+if (!app.Environment.IsDevelopment())
+{
+    app.UseHttpsRedirection();
+}
 
-app.UseAuthorization();
-
-app.MapControllers();
+// Map endpoints
+app.MapProfesionalEndpoints();
+app.MapEspecialidadEndpoints();
 
 app.Run();
