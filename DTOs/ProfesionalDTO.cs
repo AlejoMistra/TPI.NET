e43aa9baadaeb9e6a81f47592ce7ff5ef16a3c79
AlejoMistra <mistralettialejo@gmail.com>
2026-07-13 18:01:51 -0300

    Add DTOs project and initial DTO classes

---
+++ DTOs/ProfesionalDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTOs
+{
+    public class ProfesionalDTO
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; } = string.Empty;
+        public string Apellido { get; set; } = string.Empty;
+        public string Matricula { get; set; } = string.Empty;
+        public int EspecialidadId { get; set; }
+    }
+}
