f3be41c2d7e684d8ea9946990b0c15ae6dbd2a37
AlejoMistra <mistralettialejo@gmail.com>
2026-07-14 11:24:59 -0300

    Add EspecialidadService and enhance repository methods

--- Application.Services/IEspecialidadService.cs
+++ Application.Services/IEspecialidadService.cs
@@ -4,6 +4,10 @@ namespace Application.Services
 {
     public interface IEspecialidadService
     {
+        Task<EspecialidadDTO> AddAsync(EspecialidadDTO especialidadDto);
         Task<IEnumerable<EspecialidadDTO>> GetAllAsync();
+        Task<EspecialidadDTO?> GetByIdAsync(int id);
+        Task<EspecialidadDTO?> UpdateAsync(EspecialidadDTO especialidadDto);
+        Task<bool> DeleteAsync(int id);
     }
 }
